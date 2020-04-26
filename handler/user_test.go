7f21f46807131e3cadf05c3bd159f0bb3fc99b67
raahii <piyo56.net@gmail.com>
2020-04-26 18:59:20 +0900

    Add test for LoginUser. #3

--- handler/user_test.go
+++ handler/user_test.go
@@ -3,16 +3,18 @@ package handler
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/raahii/golang-grpc-realworld-example/db"
+	"github.com/raahii/golang-grpc-realworld-example/model"
 	pb "github.com/raahii/golang-grpc-realworld-example/proto"
 	"github.com/rs/zerolog"
 )
 
 func setUp(t *testing.T) (*Handler, func(t *testing.T)) {
-	w := zerolog.ConsoleWriter{Out: os.Stderr}
+	w := zerolog.ConsoleWriter{Out: ioutil.Discard}
 	l := zerolog.New(w).With().Timestamp().Logger()
 
 	d, err := db.NewTestDB()
@@ -29,7 +31,7 @@ func setUp(t *testing.T) (*Handler, func(t *testing.T)) {
 	}
 }
 
-func TestCreateuser(t *testing.T) {
+func TestCreateUser(t *testing.T) {
 	h, cleaner := setUp(t)
 	defer cleaner(t)
 
@@ -113,6 +115,65 @@ func TestCreateuser(t *testing.T) {
 		_, err := h.CreateUser(c, tt.req)
 		if (err != nil) != tt.hasError {
 			t.Errorf("%s hasError %t, but got error: %v.", tt.title, tt.hasError, err)
+		}
+	}
+}
+
+func TestLoginUser(t *testing.T) {
+	h, cleaner := setUp(t)
+	defer cleaner(t)
+
+	fooUser := model.User{
+		Username: "foo",
+		Email:    "foo@example.com",
+		Password: "secret",
+	}
+	if err := h.db.Create(&fooUser).Error; err != nil {
+		t.Fatalf("failed to create initial user record: %v", err)
+	}
+
+	tests := []struct {
+		title    string
+		req      *pb.LoginUserRequest
+		expected *model.User
+		hasError bool
+	}{
+		{
+			"success case: login foo",
+			&pb.LoginUserRequest{
+				User: &pb.User{
+					Email:    "foo@example.com",
+					Password: "secret",
+				},
+			},
+			&fooUser,
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		c := context.Background()
+		resp, err := h.LoginUser(c, tt.req)
+		if (err != nil) != tt.hasError {
+			t.Errorf("%q hasError %t, but got error: %v.", tt.title, tt.hasError, err)
+		}
+
+		if !tt.hasError {
+			if resp.User.Username != tt.expected.Username {
+				t.Errorf("%q worng Username, expected %q, got %q", tt.title, tt.expected.Username, resp.User.Username)
+			}
+			if resp.User.Email != tt.expected.Email {
+				t.Errorf("%q worng Email, expected %q, got %q", tt.title, tt.expected.Email, resp.User.Email)
+			}
+			if resp.User.Bio != tt.expected.Bio {
+				t.Errorf("%q worng Bio, expected %q, got %q", tt.title, tt.expected.Bio, resp.User.Bio)
+			}
+			if resp.User.Image != tt.expected.Image {
+				t.Errorf("%q worng Image, expected %q, got %q", tt.title, tt.expected.Image, resp.User.Image)
+			}
+			if resp.User.Token == "" {
+				t.Errorf("token must not be empety")
+			}
 		}
 	}
 }
